7da837494064cf54650719174299b3a134b3a4d2
Tillsunset <larasebastian001@gmail.com>
2022-05-19 03:35:26 -0500

    heapSort

--- Python/myshtuf/sorts/heapSort.py
+++ Python/myshtuf/sorts/heapSort.py
@@ -1,60 +1,90 @@
 import random
 import time
-import math
 
 initial = time.time()
-array = [69]
-finalArray = []
-index = 0
+array = []
 
-def siftUp(x):
-    i = len(x)-1
-    while i > 0:
-        if i%2 == 0:
-            if x[i-1] >= x[i] and x[i] < x[math.ceil(i/2)-1]:
-                x[i], x[math.ceil(i/2)-1] = x[math.ceil(i/2)-1], x[i]
-            if x[i-1] < x[i] and x[i-1] < x[math.ceil(i/2)-1]:
-                x[i-1], x[math.ceil(i/2)-1] = x[math.ceil(i/2)-1], x[i-1]
-            i= i - 2
-        else:
-            if x[i] < x[math.ceil(i/2)-1]:
-                x[i], x[math.ceil(i/2)-1] = x[math.ceil(i/2)-1], x[i]
-            i= i - 1
-    return x
+def checkSorted(x):
+	for i in range(1,len(x)):
+		if x[i - 1] > x[i]:
+			return False
+	return True
 
-def checkHeap(x):
-    global index
-    if 2*index+1 < len(x) and x[index] > x[2*index+1]:
-        return True
-    if 2*index+2 < len(x) and x[index] > x[2*index+2]:
-        return True
-    return False
+def heapify(x):
+	index = 0
+	size = len(x)
+	while (2 * index + 1 < size):
+		index += 1
+	
+	for i in range(index - 1, -1, -1):
+		while (True):
+			rightChild = 2 * i + 2
+			leftChild = 2 * i + 1
+			if (size > rightChild):
+				if (x[rightChild] > x[leftChild]):
+					if (x[i] < x[rightChild]):
+						x[i], x[rightChild] = x[rightChild], x[i]
+						i = rightChild
+					else:
+						break
+				else:
+					if (x[i] < x[leftChild]):
+						x[i], x[leftChild] = x[leftChild], x[i]
+						i = leftChild
+					else:
+						break
+			elif (size > leftChild):
+				if (x[i] < x[leftChild]):
+					x[i], x[leftChild] = x[leftChild], x[i]
+					i = leftChild
+				else:
+					break
+			else:
+				break
+	return x
 
 def siftDown(x):
-    global index
-    if 2*index + 2 < len(x) and x[2*index+1] > x[2*index+2]:
-            x[index], x[2*index+2] = x[2*index+2], x[index]
-            index = 2* index +2
-    else:
-        x[index], x[2*index+1] = x[2*index+1], x[index]
-        index = 2* index +1
-    return x
+	for i in range(len(x) - 1, 0, -1):
+		x[0], x[i] = x[i], x[0]
+		x = siftDownHelper(x,i)
+	return x
 
-while len(array) < 1000:
-    array.append(random.randint(0, 2147483647))
+def siftDownHelper(x, index):
+	i = 0
+	while (True):
+		rightChild = 2 * i + 2
+		leftChild = 2 * i + 1
+		if (index > rightChild):
+			if (x[rightChild] > x[leftChild]):
+				if (x[i] < x[rightChild]):
+					x[i], x[rightChild] = x[rightChild], x[i]
+					i = rightChild
+				else:
+					break
+			else:
+				if (x[i] < x[leftChild]):
+					x[i], x[leftChild] = x[leftChild], x[i]
+					i = leftChild
+				else:
+					break
+		elif (index > leftChild):
+			if (x[i] < x[leftChild]):
+				x[i], x[leftChild] = x[leftChild], x[i]
+				i = leftChild
+			else:
+				break
+		else:
+			break
+	return x
 
-steps = math.floor(math.log2(len(array)))
-for i in range(steps):
-    array = siftUp(array)
+while len(array) < 10000:
+    array.append(random.randint(0,100000))
 
-for i in range(len(array)-1):
-    index = 0
-    finalArray.append(array.pop(0))
-    array.insert(0, array.pop())
+array = heapify(array)
+array = siftDown(array)
 
-    while checkHeap(array):
-        array = siftDown(array)
-
-finalArray.append(array.pop(0))
 timeTaken = time.time() - initial
+# print(array)
+# print(checkSorted(array))
 print(timeTaken)
+
